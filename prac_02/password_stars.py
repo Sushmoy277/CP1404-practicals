38681f9ec79939e34b8caf9910bf58261e8a5dbf
Sushmoy277 <sushmoypaul.rohit@my.jcu.edu.au>
2025-09-27 03:45:06 +1000

    Refactor password program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,9 +1,22 @@
 MINIMUM_LENGTH = 8
 
-password = input("Enter password: ")
+def main():
+    password = get_password()
 
-while len(password) < MINIMUM_LENGTH:
-    print(f"Password must be at least {MINIMUM_LENGTH} characters long.")
+    while len(password) < MINIMUM_LENGTH:
+        print(f"Password must be at least {MINIMUM_LENGTH} characters long.")
+        password = get_password()
+
+    print_asterisks(password)
+
+
+def print_asterisks(password):
+    print("*" * len(password))
+
+
+def get_password():
     password = input("Enter password: ")
+    return password
 
-print("*" * len(password))
+
+main()
