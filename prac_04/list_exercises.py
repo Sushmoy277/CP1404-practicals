a348d23ed0767be0541fd9b61badb6ef520256d9
Sushmoy277 <sushmoypaul.rohit@my.jcu.edu.au>
2025-10-11 01:54:40 +1000

    Add username checker

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,9 +1,3 @@
-"""
-CP1404/CP5632 Practical - Suggested Solution
-List exercises
-"""
-
-# 1
 numbers = []
 is_finished = False
 while not is_finished:
@@ -21,8 +15,6 @@ print("The smallest number is", min(numbers))
 print("The largest number is", max(numbers))
 print("The average of the numbers is", sum(numbers) / len(numbers))
 
-
-# 2.
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye',
              'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command',
              'ExecState', 'InteractiveConsole', 'InterpreterInterface',
