2715658f0fb19a0418e627ecf929cb0ea3ca88f8
Sushmoy277 <sushmoypaul.rohit@my.jcu.edu.au>
2025-10-12 02:25:57 +1000

    Add exception for error check of month and income

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -7,12 +7,27 @@ Starter code for cumulative total income program
 def main():
     """Display income report for incomes over a given number of number_of_months."""
     incomes = []
-    number_of_months = int(input("How many number of months? "))
+    is_finished = False
+    while not is_finished:
+        try:
+            number_of_months = int(input("How many number of months? "))
+            if number_of_months <= 0:
+                print("Number of months must be greater than 0!")
+            is_finished = True
+        except ValueError:
+            print("Please enter valid integer!")
 
     for month in range(1, number_of_months + 1):
-        income = float(input(f"Enter income for month {month}: "))
-        incomes.append(income)
-
+        is_finished = False
+        while not is_finished:
+            try:
+                income = float(input(f"Enter income for month {month}:$ "))
+                if income <= 0:
+                    print("Income must be greater than 0!")
+                incomes.append(income)
+                is_finished = True
+            except ValueError:
+                print("Please enter valid integer!")
     print_report(incomes, number_of_months)
 
 
@@ -23,7 +38,6 @@ def print_report(incomes, number_of_months: int):
         income = incomes[month - 1]
         total += income
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
-
 
 
 main()
