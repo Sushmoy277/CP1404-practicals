06ad145c3c100a28fe5c022391240a3d4b629982
Sushmoy277 <sushmoypaul.rohit@my.jcu.edu.au>
2025-10-18 02:31:16 +1000

    Complete prac 5 state names exercise

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -3,15 +3,29 @@ CP1404/CP5632 Practical
 State names in a dictionary
 File needs reformatting
 """
+from operator import itemgetter
 
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
-                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
+CODE_TO_NAME = {
+    "QLD": "Queensland",
+    "NSW": "New South Wales",
+    "NT": "Northern Territory",
+    "WA": "Western Australia",
+    "ACT": "Australian Capital Territory",
+    "VIC": "Victoria",
+    "TAS": "Tasmania",
+    "SA": "South Australia"
+}
+
 print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ")
+state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+    try:
+        print(f"{state_code} is {CODE_TO_NAME[state_code]}")
+    except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state_code = input("Enter short state: ").upper()
+data = list(CODE_TO_NAME.items())
+max_length = max(len(name) for name in CODE_TO_NAME)
+for code, name in sorted(data, reverse=True, key=itemgetter(1, 0)):
+    print(f"{code:3} is {name}")
