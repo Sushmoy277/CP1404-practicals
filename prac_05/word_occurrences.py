3aa4721a197d5d3fcb4d3b7a39b4b02152264277
Sushmoy277 <sushmoypaul.rohit@my.jcu.edu.au>
2025-10-18 03:27:28 +1000

    Add word count

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -3,3 +3,15 @@ Word Occurrences
 Estimate: 30 minutes
 Actual:   minutes
 """
+
+word_to_count = {}
+text = input("Text: ")
+words = text.split()
+for word in words:
+    try:
+        word_to_count[word] += 1
+    except KeyError:
+        word_to_count[word] = 1
+max_length = max(len(word) for word in words)
+for word in sorted(word_to_count):
+    print(f"{word:{max_length}} : {word_to_count[word]}")
