645db43ad3702e1fbd381bcb8654d4d076e23801
Sushmoy277 <sushmoypaul.rohit@my.jcu.edu.au>
2025-11-01 01:51:43 +1000

    Add __init__

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -2,3 +2,10 @@
 Estimate: 25 minutes
 Actual:    minutes
 """
+class ProgrammingLanguage:
+    def __init__(self, name= "",typing = "", reflection = "", year = ""):
+        """Create a ProgrammingLanguage with name, typing , reflection , year."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
