a016157433f7818522bf0788d9da6bed06364b2c
Sushmoy277 <sushmoypaul.rohit@my.jcu.edu.au>
2025-11-10 23:05:54 +1000

    Add Project class with basic attributes and constructor

---
+++ prac_07/project.py
@@ -0,0 +1,13 @@
+"""CP1404/CP5632 Practical - Project class"""
+
+
+
+class Project:
+    """Represent a simple project."""
+
+    def __init__(self, name, start_date, priority, cost_estimate, completion):
+        self.name = name
+        self.start_date = start_date
+        self.priority = int(priority)
+        self.cost_estimate = float(cost_estimate)
+        self.completion = int(completion)
