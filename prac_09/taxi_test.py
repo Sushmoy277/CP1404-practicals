b556a199feae9c5bad915486789146de7ca02dcc
Sushmoy277 <sushmoypaul.rohit@my.jcu.edu.au>
2025-11-23 19:29:09 +1000

    Update taxi test

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -5,7 +5,7 @@ from taxi import Taxi
 
 def main():
     """Test the Taxi class using the required steps."""
-    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi = Taxi("Prius 1", 100)
     my_taxi.drive(40)
     print(my_taxi)
     print(f"Current fare: ${my_taxi.get_fare():.2f}")
